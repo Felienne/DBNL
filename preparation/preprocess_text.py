1ac34527ea4ab07d1490155ebc60c96dcb2effa0
Felienne Hermans <felienne@gmail.com>
2025-03-02 19:22:09 +0100

    search for multiple words

--- preparation/preprocess_text.py
+++ preparation/preprocess_text.py
@@ -9,8 +9,8 @@ nlp.add_pipe('sentencizer')
 nlp.max_length = 6_000_000
 
 
-def select_sentences(paths, searchterm):
-    "Select sentences from files located in PATHS that include search word."
+def select_sentences(paths, searchterms):
+    "Select sentences from files located in PATHS that include one of the search words."
     sent_index = dict()
     for path in paths:
         print(path)
@@ -19,13 +19,14 @@ def select_sentences(paths, searchterm):
         doc = nlp(text)
         relevant_sentences = []
         for sentence in doc.sents:
-            if searchterm in sentence.text:
-                relevant_sentences.append(sentence.text)
+            for searchterm in searchterms:
+                if searchterm in sentence.text:
+                    relevant_sentences.append(sentence.text)
         sent_index[base_name] = relevant_sentences
     return sent_index
 
 
 paths = glob.glob('./proza/*.epub')
-sent_index = select_sentences(paths, 'vrouw')
+sent_index = select_sentences(paths, ['vrouw', 'man'])
 with open('resources/selected_sentences.json','w') as f:
     json.dump(sent_index, f, indent=4)
